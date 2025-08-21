3e5f6dd24bf3a67f10db8ce793a5cb903f44ecd6
Juanito <juanprieto1627@gmail.com>
2025-08-21 16:22:59 -0500

    v0.0_Versioninicial

---
+++ src/main/java/com/sena/techaccess/TechAccess3063267Application.java
@@ -0,0 +1,13 @@
+package com.sena.techaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAccess3063267Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAccess3063267Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/techaccess/TechAccess3063267ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.techaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAccess3063267ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
