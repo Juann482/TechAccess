35799c096ad7f152407b2e0c05885b828566644b
greily <camargogreily07@gmail.com>
2025-08-22 16:57:24 -0500

    v.0.0.1_relanzamiento

---
+++ src/main/java/com/sena/techaccess/TechAccessApplication.java
@@ -0,0 +1,13 @@
+package com.sena.techaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/techaccess/TechAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.techaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
