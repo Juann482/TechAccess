5113fcc61efdf32dfcd58d5fcf2493d4391b9d09
berthus <bernardosan13@gmail.com>
2025-08-29 13:40:46 -0500

    CreacionUsuariomodel&Rolmodel_29082025

--- src/main/java/com/sena/techaccess/model/Rol.java
+++ src/main/java/com/sena/techaccess/model/Rol.java
@@ -1,5 +1,51 @@
 package com.sena.techaccess.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Rol")
+
 public class Rol {
+
+	private Integer id;
+	private String tipo;
+
+	@OneToMany(mappedBy = "Rol")
+	private List<Usuario> usuario;
+
+	public Rol() {
+
+	}
+
+	public Rol(Integer id, String tipo) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Override
+	public String toString() {
+		return "Rol [id=" + id + ", tipo=" + tipo + "]";
+	}
 
 }
