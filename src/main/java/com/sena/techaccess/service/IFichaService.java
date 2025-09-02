75aeec5e814fe6178890f1a7aa2c79076f167388
greily <camargogreily07@gmail.com>
2025-09-02 14:48:08 -0500

    v0.2_creaciónFichaPaqueteModel&interfazdeFichaService_greily_02092025

--- src/main/java/com/sena/techaccess/service/IFichaService.java
+++ src/main/java/com/sena/techaccess/service/IFichaService.java
@@ -1,5 +1,26 @@
 package com.sena.techaccess.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.sena.techaccess.model.Ficha;
+import com.sena.techaccess.model.Usuario;
+
 public interface IFichaService {
 
+	// CRUD
+	Ficha save(Ficha ficha); // Crear o actualizar
+
+	void delete(Integer idFicha); // Eliminar
+
+	Optional<Ficha> findById(Integer idFicha); // Buscar por ID
+
+	List<Ficha> findAll(); // Listar todas las fichas
+
+	// Consultas personalizadas
+	List<Ficha> findByNombrePrograma(String nombrePrograma);
+
+	Ficha findByNuumFicha(String nuumFicha);
+
+	List<Usuario> findUsuariosByIdFicha(Integer idFicha);
 }
